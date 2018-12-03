dfaf179e953ba1a27efc085f2b44978cc796a723
Mateusz Kyc <mateusz.kyc@gmail.com>
2018-12-03 22:11:50 +0100

    init

---
+++ src/test/java/it/mltk/kes/KesApplicationTests.java
@@ -0,0 +1,16 @@
+package it.mltk.kes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
